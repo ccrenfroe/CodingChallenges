4d88365da88236963f8778647e0339275f0b3d78
Caleb Renfroe <caleb.renfroe007@gmail.com>
2024-01-15 16:23:27 -0500

    Added line count functionality
    ccwc.c
     + count_lines function added - counts the number of lines from a given
       given file.
     * main - Added a check to see if any of the flags (bytes_flag, lines_flag, or
       words,flag) are true.

--- ccwc/ccwc.c
+++ ccwc/ccwc.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <getopt.h>
 #include <stdbool.h>
+#include <limits.h>
 
 bool bytes_flag,lines_flag, words_flag;
 int arg_in;
@@ -30,6 +31,27 @@ int count_bytes(const char *filename){
 	return byte_count;
 }
 
+int count_lines(const char *filename){
+	FILE * file_stream;
+	char * line_buffer;
+	size_t line_size = LINE_MAX;
+	int line_count = 0;
+
+	file_stream = fopen(filename, "r");
+
+	if (file_stream == NULL) {
+		printf("File count not be opened.\n");
+	}
+	else{
+		line_buffer = (char *)malloc(line_size);
+		while (getline(&line_buffer, &line_size, file_stream) != EOF){
+			line_count++;
+		}
+		fclose(file_stream);
+	}
+	return line_count;
+}
+
 void print_file_results(const int bytes, const int lines, const int  words, const char *filename){
 	if (lines != 0) { 
 		printf("%d ", lines);
@@ -44,7 +66,7 @@ void print_file_results(const int bytes, const int lines, const int  words, cons
 	return;
 }
 
-int main (int argc, char * argv[]) {
+int main (int argc, char * argv[]){
 
 	while(1){
 		arg_in = getopt_long (argc, argv, "cl", long_opts, NULL);
@@ -60,34 +82,40 @@ int main (int argc, char * argv[]) {
 				lines_flag = true;
 				break;
 			default:
-				abort();
+				exit(1);
 		}
 	}
+	
+	if (bytes_flag | lines_flag | words_flag){
+		if (optind < argc){
+			while (optind < argc){
+				int bytes_total = 0;
+				int lines_total = 0;
+				int words_total = 0;
 
-	if (optind < argc){
-		while (optind < argc){
-			int bytes_total = 0;
-			int lines_total = 0;
-			int words_total = 0;
+				if ( bytes_flag ){
+					bytes_total = count_bytes(argv[optind]);
+				}
+				if (lines_flag){
+					lines_total = count_lines(argv[optind]);
+				}
+				if (words_flag){
+					break;
+				}
 
-			if ( bytes_flag ){
-				bytes_total = count_bytes(argv[optind]);
+				print_file_results(bytes_total, lines_total, words_total, argv[optind]);
+				optind++;
 			}
-			if (lines_flag){
-				break;
-			}
-			if (words_flag){
-				break;
-			}
-
-			print_file_results(bytes_total, lines_total, words_total, argv[optind]);
-			optind++;
+		}
+		else{
+			printf("No files given\n");
+			exit(1);
 		}
 	}
-	else {
-		printf("No files given\n");
+	else{
+		printf("No parameters given.\n");
 		exit(1);
 	}
-	exit(0);
+		exit(0);
 }
 
